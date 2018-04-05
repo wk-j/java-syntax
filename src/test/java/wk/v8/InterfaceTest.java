cb6b5418caa7ccd1a59ff17334eecd0c7ab8fc9b
wk-j <somnuk.wk@gmail.com>
2018-04-05 15:11:00 +0700

    Method reference

--- src/test/java/wk/v8/InterfaceTest.java
+++ src/test/java/wk/v8/InterfaceTest.java
@@ -1,17 +1,17 @@
-package wk;
+package wk.v8;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MyInterfaceTest {
+public class InterfaceTest {
     @Test
     public void staticMethod() {
-        String a = MyInterface.producer();
+        String a = Interface.producer();
         Assert.assertEquals(a, "N&F Vehicles");
     }
 }
 
-interface MyInterface {
+interface Interface {
     public static String producer() {
         return "N&F Vehicles";
     }
---
+++ src/test/java/wk/v8/MethodReferenceTest.java
@@ -0,0 +1,53 @@
+package wk.v8;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MethodReferenceTest {
+
+    @Test
+    public void staticTest() {
+        Collection<String> emails = Arrays.asList("a", "b", "a@c.com");
+        Stream<String> r1 = emails.stream().filter(User::isEmail);
+        Assert.assertEquals(1, r1.count());
+    }
+
+    @Test
+    public void instanceTest() {
+        Collection<String> emails = Arrays.asList("a", "b", "a@c.com");
+        User user = new User();
+        Stream<String> r2 = emails.stream().filter(user::isEmail2);
+        Assert.assertEquals(1, r2.count());
+    }
+
+    @Test
+    public void constructorTest() {
+        Collection<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<User> users = data.stream().map(User::new);
+        Assert.assertEquals(5, users.count());
+    }
+}
+
+class User {
+
+    private int age;
+
+    public User() {
+    }
+
+    public User(int age) {
+        this.age = age;
+    }
+
+    public static boolean isEmail(String token) {
+        return token.contains("@");
+    }
+
+    public boolean isEmail2(String token) {
+        return token.contains("@");
+    }
+}
