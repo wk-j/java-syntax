2b1ebee621fef2e04142eadac09776d86199eb63
wk-j <somnuk.wk@gmail.com>
2018-04-10 12:42:18 +0700

    Deep equals

---
+++ src/test/java/wk/v5/DeepEqualsTest.java
@@ -0,0 +1,23 @@
+package wk.v5;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DeepEqualsTest {
+    @Test
+    public void equalsTests() {
+        Object[] a1 = new Object[] { 1, 2, 3 };
+        Object[] a2 = new Object[] { 1, 2, 3 };
+        boolean equal = Arrays.deepEquals(a1, a2);
+        Assert.assertTrue(equal);
+    }
+
+    @Test
+    public void stringEqualsTest() {
+        Object[] a1 = new Object[] { "A", "B" };
+        Object[] a2 = new Object[] { "A", "B" };
+        boolean equal = Arrays.deepEquals(a1, a2);
+        Assert.assertTrue(equal);
+    }
+}
